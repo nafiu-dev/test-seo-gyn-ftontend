dfb373c0a52e59dad4ec04c5a197b6b2adad7297
nafiu-dev <nafiu.developer@gmail.com>
2022-10-16 21:38:15 +0500

    headers updated

--- src/utils/SetauthToken.js
+++ src/utils/SetauthToken.js
@@ -2,6 +2,8 @@ import axios from 'axios'
 const SetauthToken = token => {
     if(token){
         axios.defaults.headers.common['x-auth-token'] = token
+        axios.defaults.headers.common['Access-Control-Allow-Origin'] = '*'
+        axios.defaults.headers.common['Access-Control-Allow-Methods'] = 'GET,PUT,POST,DELETE,PATCH,OPTIONS'
     }else{
         delete axios.defaults.headers.common['x-auth-token']
     }
